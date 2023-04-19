b168ed29f7a6e2712c1b4ed485b6a596aa04eafa
Eniter <jakot@student.agh.edu.pl>
2023-04-19 12:24:11 +0200

    Added the variables listener and verifier

--- Script.py
+++ Script.py
@@ -1,14 +1,44 @@
-from antlr4 import FileStream, CommonTokenStream
+from antlr4 import FileStream, CommonTokenStream, ParseTreeWalker, ParseTreeListener, Token
 from antlr4.error.ErrorListener import ErrorListener
 import sys
 # sys.path.append('antlr')
 from antlr.JailBreakLangLexer import JailBreakLangLexer
 from antlr.JailBreakLangParser import JailBreakLangParser
 from antlr.JailBreakLangVisitorCustom import JailBreakLang
+from antlr.JailBreakLangListenerCustom import JailBreakLangListenerCustom, IDListenerLexer
+from antlr.JailBreakLangLexer import JailBreakLangLexer
+
+import warnings
 
 class MyErrorListener(ErrorListener):
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
         raise ValueError(f"Syntax error on line {line}, column {column}: {msg}")
+
+class IDVisitor(ParseTreeListener):
+    #def __init__(self):
+    #    self.ids = set()
+
+    def visitTerminal(self, node):
+        if node.symbol.type == JailBreakLangLexer.ID:
+            if node.symbol.text not in set_of_vars:
+                warnings.warn("Cannot use undeclared variable")
+                exit()
+                
+        #self.ids.add(node.symbol.text)
+
+def find_ids():
+    input_stream = FileStream(sys.argv[1])
+    lexer = JailBreakLangLexer(input_stream)
+    token_stream = CommonTokenStream(lexer)
+    parser = JailBreakLangParser(token_stream)
+    tree = parser.start()
+
+    visitor = IDVisitor()
+    walker = ParseTreeWalker()
+    walker.walk(visitor, tree)
+
+    #return visitor.ids
+
 
 
 input_stream = FileStream(sys.argv[1])
@@ -16,9 +46,23 @@ lexer = JailBreakLangLexer(input_stream)
 token_stream = CommonTokenStream(lexer)
 parser = JailBreakLangParser(token_stream)
 
+
+
 # Add the error listener to the parser
 error_listener = MyErrorListener()
 parser.addErrorListener(error_listener)
 tree = parser.start()
+
+# First walk
+listener = JailBreakLangListenerCustom()
+walker = ParseTreeWalker()
+walker.walk(listener, tree)
+set_of_vars = listener.exitProgram()
+
+# Second walk
+find_ids()
+
+# Actual compiling
+lexer = JailBreakLangLexer(input_stream)
 visitor = JailBreakLang()
 visitor.visit(tree)
---
+++ antlr/JailBreakLangListenerCustom.py
@@ -0,0 +1,170 @@
+# Generated from JailBreakLang.g4 by ANTLR 4.12.0
+from antlr4 import *
+if __name__ is not None and "." in __name__:
+    from .JailBreakLangParser import JailBreakLangParser
+    from .JailBreakLangLexer import JailBreakLangLexer
+else:
+    from JailBreakLangParser import JailBreakLangParser
+    from JailBreakLangLexer import JailBreakLangLexer
+
+
+
+# This class defines a complete listener for a parse tree produced by JailBreakLangParser.
+class JailBreakLangListenerCustom(ParseTreeListener):
+    
+    def __init__(self):
+        self.declared_vars = set()
+
+    def exitProgram(self):
+        return self.declared_vars
+
+    # Enter a parse tree produced by JailBreakLangParser#start.
+    def enterStart(self, ctx:JailBreakLangParser.StartContext):
+        pass
+
+    # Exit a parse tree produced by JailBreakLangParser#start.
+    def exitStart(self, ctx:JailBreakLangParser.StartContext):
+        pass
+
+
+    # Enter a parse tree produced by JailBreakLangParser#code.
+    def enterCode(self, ctx:JailBreakLangParser.CodeContext):
+        pass
+
+    # Exit a parse tree produced by JailBreakLangParser#code.
+    def exitCode(self, ctx:JailBreakLangParser.CodeContext):
+        pass
+
+
+    # Enter a parse tree produced by JailBreakLangParser#objects.
+    def enterObjects(self, ctx:JailBreakLangParser.ObjectsContext):
+        pass
+
+    # Exit a parse tree produced by JailBreakLangParser#objects.
+    def exitObjects(self, ctx:JailBreakLangParser.ObjectsContext):
+        pass
+
+    # def enterEveryRule(self, ctx):
+    #     for i in ctx.getChildren():
+    #         print('### ', i, ' $$$')
+
+
+
+    # Enter a parse tree produced by JailBreakLangParser#variables.
+    def enterVariables(self, ctx): 
+        #print(ctx.getChildCount(), ctx.getChild(0).getText())
+        if ctx.getChild(0).getText() == 'INT' or ctx.getChild(0).getText() == 'BOOLEAN':
+            new_var = ctx.getChild(1).getText()
+            self.declared_vars.add(new_var)
+
+    # Exit a parse tree produced by JailBreakLangParser#variables.
+    def exitVariables(self, ctx:JailBreakLangParser.VariablesContext):
+        pass
+
+
+    # Enter a parse tree produced by JailBreakLangParser#comparison.
+    def enterComparison(self, ctx:JailBreakLangParser.ComparisonContext):
+        pass
+
+    # Exit a parse tree produced by JailBreakLangParser#comparison.
+    def exitComparison(self, ctx:JailBreakLangParser.ComparisonContext):
+        pass
+
+
+    # Enter a parse tree produced by JailBreakLangParser#expr.
+    def enterExpr(self, ctx:JailBreakLangParser.ExprContext):
+        pass
+
+    # Exit a parse tree produced by JailBreakLangParser#expr.
+    def exitExpr(self, ctx:JailBreakLangParser.ExprContext):
+        pass
+
+
+    # Enter a parse tree produced by JailBreakLangParser#term.
+    def enterTerm(self, ctx:JailBreakLangParser.TermContext):
+        pass
+
+    # Exit a parse tree produced by JailBreakLangParser#term.
+    def exitTerm(self, ctx:JailBreakLangParser.TermContext):
+        pass
+
+
+    # Enter a parse tree produced by JailBreakLangParser#factor.
+    def enterFactor(self, ctx:JailBreakLangParser.FactorContext):
+        pass
+
+    # Exit a parse tree produced by JailBreakLangParser#factor.
+    def exitFactor(self, ctx:JailBreakLangParser.FactorContext):
+        pass
+
+
+    # Enter a parse tree produced by JailBreakLangParser#commands.
+    def enterCommands(self, ctx:JailBreakLangParser.CommandsContext):
+        pass
+
+    # Exit a parse tree produced by JailBreakLangParser#commands.
+    def exitCommands(self, ctx:JailBreakLangParser.CommandsContext):
+        pass
+
+
+    # Enter a parse tree produced by JailBreakLangParser#function_declaration.
+    def enterFunction_declaration(self, ctx:JailBreakLangParser.Function_declarationContext):
+        pass
+
+    # Exit a parse tree produced by JailBreakLangParser#function_declaration.
+    def exitFunction_declaration(self, ctx:JailBreakLangParser.Function_declarationContext):
+        pass
+
+
+    # Enter a parse tree produced by JailBreakLangParser#expressions.
+    def enterExpressions(self, ctx:JailBreakLangParser.ExpressionsContext):
+        pass
+
+    # Exit a parse tree produced by JailBreakLangParser#expressions.
+    def exitExpressions(self, ctx:JailBreakLangParser.ExpressionsContext):
+        pass
+
+
+    # Enter a parse tree produced by JailBreakLangParser#guard_extra_code.
+    def enterGuard_extra_code(self, ctx:JailBreakLangParser.Guard_extra_codeContext):
+        pass
+
+    # Exit a parse tree produced by JailBreakLangParser#guard_extra_code.
+    def exitGuard_extra_code(self, ctx:JailBreakLangParser.Guard_extra_codeContext):
+        pass
+
+
+    # Enter a parse tree produced by JailBreakLangParser#guard_control.
+    def enterGuard_control(self, ctx:JailBreakLangParser.Guard_controlContext):
+        pass
+
+    # Exit a parse tree produced by JailBreakLangParser#guard_control.
+    def exitGuard_control(self, ctx:JailBreakLangParser.Guard_controlContext):
+        pass
+
+
+    # Enter a parse tree produced by JailBreakLangParser#booleanValue.
+    def enterBooleanValue(self, ctx:JailBreakLangParser.BooleanValueContext):
+        pass
+
+    # Exit a parse tree produced by JailBreakLangParser#booleanValue.
+    def exitBooleanValue(self, ctx:JailBreakLangParser.BooleanValueContext):
+        pass
+
+
+    # Enter a parse tree produced by JailBreakLangParser#condition.
+    def enterCondition(self, ctx:JailBreakLangParser.ConditionContext):
+        pass
+
+    # Exit a parse tree produced by JailBreakLangParser#condition.
+    def exitCondition(self, ctx:JailBreakLangParser.ConditionContext):
+        pass
+
+class IDListenerLexer(JailBreakLangLexer):
+    def nextToken(self):
+        token = super().nextToken()
+        if token.type == JailBreakLangLexer.ID:
+            print("Found ID: " + token.text)
+        return token
+
+del JailBreakLangParser
